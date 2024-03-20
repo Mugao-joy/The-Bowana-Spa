dbf18587450e40841ecb5b81e3c42345444d07b7
Mugao-joy <mugaojoy@gmail.com>
2024-03-20 18:30:08 +0300

    styled booking form to look more appealing

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,84 +1,109 @@
 import React, { useState } from 'react';
-//name, phone, email, date, time, special request
-function BookingForm({ onBookAppointment }) {
-    const [name, setName] = useState('')
-    const [phoneNumber, setPhoneNumber] = useState('')
-    const [email, setEmail] = useState('')
-    const [date, setDate] = useState('')
-    const [time, setTime] = useState('')
-    const [comments, setComments] = useState('')
+import Navbar from './Navbar';
+import './bookingform.css'
+//import { Link } from 'react-router-dom';
+//name,phone,email,time,date,special request comments
 
-    const handleSubmit = (event) => {//will be called when form is submitted
-        event.preventDefault()
-        onBookAppointment({ name, phoneNumber, email, date, time, comments });
-        // Reset form fields after submission
-        setName('')
-        setPhoneNumber('')
-        setEmail('')
-        setDate('')
-        setTime('')
-        setComments('')
-        // Reload the pag
-        window.location.reload()
-  }
+function Booking({ onBookAppointment }) {
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [email, setEmail] = useState('')
+  const [date, setDate] = useState('')
+  const [time, setTime] = useState('')
+  const [comments, setComments] = useState('')
 
-    return (
-        <form onSubmit={handleSubmit}>
-        <div>
-            <label>Name:</label>
-            <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            />
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onBookAppointment({ name, phoneNumber, email, date, time, comments });
+    // Reset form fields after submission
+    setName('')
+    setPhoneNumber('')
+    setEmail('')
+    setDate('')
+    setTime('')
+    setComments('')
+    // Reload the page
+    window.location.href = '/'
+
+}
+
+return (
+    <div>
+        <Navbar />
+      
+        <div className="form-container">
+            <h2 className="form-title">FILL THIS FORM TO BOOK AN APPOINTMENT</h2>
+            <form onSubmit={handleSubmit}>
+            {/* Name field */}
+            <div>
+                <label>Name:</label>
+                <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+                />
+            </div>
+
+            {/* Phone number field */}
+            <div>
+                <label>Phone Number:</label>
+                <input
+                type="tel"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                required
+                />
+            </div>
+
+            {/* Email field */}
+            <div>
+                <label>Email:</label>
+                <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                />
+            </div>
+
+            {/* Date field */}
+            <div>
+                <label>Date of Appointment:</label>
+                <input
+                type="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                required
+                />
+            </div>
+
+            {/* Time field */}
+            <div>
+                <label>Time of Appointment:</label>
+                <input
+                type="time"
+                value={time}
+                onChange={(e) => setTime(e.target.value)}
+                required
+                />
+            </div>
+
+            {/* Comments section */}
+            <div>
+                <label>Comments/Special Requests:</label>
+                <textarea
+                value={comments}
+                onChange={(e) => setComments(e.target.value)}
+                />
+            </div>
+
+            {/* Submit button */}
+            <button type="submit">Book Appointment</button>
+            </form>
         </div>
-        <div>
-            <label>Phone Number:</label>
-            <input
-            type="tel"
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            required
-            />
         </div>
-        <div>
-            <label>Email:</label>
-            <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            />
-        </div>
-        <div>
-            <label>Date:</label>
-            <input
-            type="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-            required
-            />
-        </div>
-        <div>
-            <label>Time:</label>
-            <input
-            type="time"
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
-            required
-            />
-        </div>
-        <div>
-            <label>Comments/Special Requests:</label>
-            <textarea
-            value={comments}
-            onChange={(e) => setComments(e.target.value)}
-            />
-        </div>
-        <button type="submit">Book Appointment</button>
-        </form>
     )
 }
 
-export default BookingForm
+export default Booking

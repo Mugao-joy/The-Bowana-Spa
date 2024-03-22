022477ceecaf4959457651a1e06416fb6a0d10b7
Mugao-joy <mugaojoy@gmail.com>
2024-03-22 19:44:48 +0300

    modifications

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,8 +6,8 @@ const Landing = () => {
     return (
         <div className="landing-bg">
             <div className="landing-container text-white">
-                <h1 className="lg:text-6xl md:text-4xl text-2xl font-semibold mb-4">50% OFF</h1>
-                <p className="mb-6 lg:p-5">On your first appointment</p>
+                <h1 className="lg:text-6xl md:text-4xl text-2xl font-semibold mb-4">Indulge in Tranquility</h1>
+                <p className="mb-6 lg:p-5">Discover our opulent oasis of relaxation</p>
                 <a href="/bookingform" className="bg-pink-500 hover:bg-pink-600 text-white py-2 px-6 rounded-full text-lg hover:transition duration-300 ease-in-out">Book Now</a>
             </div>
         </div>
